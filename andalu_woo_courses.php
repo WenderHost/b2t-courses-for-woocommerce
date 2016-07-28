4b4a70596f23d649ffb1d99838b29722b54db1c9
Michael Wender <michael@michaelwender.com>
2016-07-28 14:30:14 -0400

    Adding Mailtrap integration

--- andalu_woo_courses.php
+++ andalu_woo_courses.php
@@ -16,14 +16,14 @@ if ( ! class_exists( 'Andalu_Woo_Courses' ) ) :
 class Andalu_Woo_Courses {
 
 	public static $url, $dir;
-	
+
 	public static $product_type = 'course';
 	public static $endorsements = array( 'PMI', 'IIBA' );
-	
+
 	static function init() {
 		self::$url = plugins_url('', __FILE__);
 		self::$dir = plugin_dir_path(__FILE__);
-		
+
 		load_plugin_textdomain( 'andalu_woo_courses', false, self::$dir . '/languages' );
 
 		if ( is_admin() ) {
@@ -42,7 +42,11 @@ class Andalu_Woo_Courses {
 		require_once( 'includes/class-course-product.php' );
 		require_once( 'includes/class-course-class-product.php' );
 	}
-	
+
 }
 Andalu_Woo_Courses::init();
 endif;
+
+$mailtrap = dirname( __FILE__ ) . '/mailtrap.php';
+if( file_exists( $mailtrap ) )
+    require( $mailtrap );
