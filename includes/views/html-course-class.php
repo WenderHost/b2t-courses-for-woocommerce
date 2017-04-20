315ce7c2435a27b2c77d8287455f7d5bc6eebcd8
Michael Wender <michael@michaelwender.com>
2017-04-20 11:57:53 -0400

    Upating new class form to work with WC3.0+

--- includes/views/html-course-class.php
+++ includes/views/html-course-class.php
@@ -1,8 +1,40 @@
+<?php
+$atts = [
+	'position',
+	'id',
+	'time',
+	'seats',
+	'id',
+	'end_date',
+	'start_date',
+	'location',
+	'confirmed'
+];
+
+foreach( $atts as $att ){
+	if( isset( $class->$att ) ){
+		switch( $att ){
+			case 'location':
+				$class_location = $class->location;
+			break;
+			case 'id':
+				$$att = $class->get_id();
+			break;
+			default:
+				$$att = $class->$att;
+			break;
+		}
+	} else {
+		$$att = '';
+	}
+}
+
+?>
 <div class="course_class wc-metabox closed" rel="<?php echo $class->position; ?>">
 	<h3>
 		<a href="#" class="remove_row delete"><?php _e( 'Remove', 'andalu_woo_courses' ); ?></a>
 		<div class="handlediv" title="<?php esc_attr_e( 'Click to toggle', 'andalu_woo_courses' ); ?>"></div>
-		<strong class="class_date"><?php echo esc_html( $class->start_date . ( empty( $class->end_date ) ? '' : ' &ndash; ' . $class->end_date ) ); ?></strong>
+		<strong class="class_date"><?php echo esc_html( $start_date . ( empty( $end_date ) ? '' : ' &ndash; ' . $end_date ) ); ?></strong>
 	</h3>
 	<div class="course_class_data wc-metabox-content">
 		<table cellpadding="0" cellspacing="0">
@@ -10,17 +42,17 @@
 				<tr>
 					<td class="class_start_date">
 						<label><?php _e( 'Date', 'andalu_woo_courses' ); ?>:</label>
-						<input type="text" class="class_start_date course_date" name="classes[<?php echo $i; ?>][start_date]" value="<?php echo esc_attr( $class->start_date ); ?>" placeholder="YYYY-MM-DD" maxlength="10" pattern="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])" />
+						<input type="text" class="class_start_date course_date" name="classes[<?php echo $i; ?>][start_date]" value="<?php echo esc_attr( $start_date ); ?>" placeholder="YYYY-MM-DD" maxlength="10" pattern="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])" />
 						<span class="seperator">&ndash;</span>
-						<input type="text" class="class_end_date course_date" name="classes[<?php echo $i; ?>][end_date]" value="<?php echo esc_attr( $class->end_date ); ?>" placeholder="YYYY-MM-DD" maxlength="10" pattern="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])" />
-						<input type="hidden" name="classes[<?php echo $i; ?>][position]" class="class_position" value="<?php echo esc_attr( $class->position ); ?>" />
-						<input type="hidden" name="classes[<?php echo $i; ?>][id]" value="<?php echo esc_attr( $class->get_id() ); ?>" />
+						<input type="text" class="class_end_date course_date" name="classes[<?php echo $i; ?>][end_date]" value="<?php echo esc_attr( $end_date ); ?>" placeholder="YYYY-MM-DD" maxlength="10" pattern="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])" />
+						<input type="hidden" name="classes[<?php echo $i; ?>][position]" class="class_position" value="<?php echo esc_attr( $position ); ?>" />
+						<input type="hidden" name="classes[<?php echo $i; ?>][id]" value="<?php echo esc_attr( $id ); ?>" />
 					</td>
 				</tr>
 				<tr>
 					<td class="class_time">
 						<label><?php _e( 'Time', 'andalu_woo_courses' ); ?>:</label>
-						<input type="text" class="class_time" name="classes[<?php echo $i; ?>][time]" value="<?php echo esc_attr( $class->time ); ?>" />
+						<input type="text" class="class_time" name="classes[<?php echo $i; ?>][time]" value="<?php echo esc_attr( $time ); ?>" />
 					</td>
 				</tr>
 				<tr>
@@ -28,7 +60,7 @@
 						<label><?php _e( 'Location', 'andalu_woo_courses' ); ?>:</label>
 						<select class="class_location" name="classes[<?php echo $i; ?>][location]">
 						<?php foreach( $locations as $location_id => $location ) : ?>
-							<option value="<?php echo $location_id; ?>" <?php selected( $location_id, $class->location ); ?>><?php echo $location; ?></option>
+							<option value="<?php echo $location_id; ?>" <?php selected( $location_id, $class_location ); ?>><?php echo $location; ?></option>
 						<?php endforeach; ?>
 						</select>
 					</td>
@@ -36,12 +68,12 @@
 				<tr>
 					<td class="class_seats">
 						<label><?php _e( 'Seats available', 'andalu_woo_courses' ); ?>:</label>
-						<input type="number" class="class_seats" name="classes[<?php echo $i; ?>][seats]" value="<?php echo esc_attr( $class->seats ); ?>" />
+						<input type="number" class="class_seats" name="classes[<?php echo $i; ?>][seats]" value="<?php echo esc_attr( $seats ); ?>" />
 					</td>
 				</tr>
 				<tr>
 					<td class="class_confirmed">
-						<input type="checkbox" class="class_confirmed" name="classes[<?php echo $i; ?>][confirmed]" <?php checked( $class->confirmed ); ?> />
+						<input type="checkbox" class="class_confirmed" name="classes[<?php echo $i; ?>][confirmed]" <?php checked( $confirmed ); ?> />
 						<span><?php _e( 'Has this class been confirmed?', 'andalu_woo_courses' ); ?></span>
 					</td>
 				</tr>
