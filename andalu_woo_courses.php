f629dfdab9670d3fe1bf345ee06305ae47aa553b
Michael Wender <michael@michaelwender.com>
2020-12-02 10:53:41 -0500

    Adding `uber_log()` for debugging

--- andalu_woo_courses.php
+++ andalu_woo_courses.php
@@ -73,6 +73,25 @@ class Andalu_Woo_Courses {
 Andalu_Woo_Courses::init();
 endif;
 
+if( ! function_exists( 'uber_log' ) ){
+	/**
+	 * Enhanced logging.
+	 *
+	 * @param      string  $message  The log message
+	 */
+	function uber_log( $message = null ){
+	  static $counter = 1;
+
+	  $bt = debug_backtrace();
+	  $caller = array_shift( $bt );
+
+	  if( 1 == $counter )
+	    error_log( "\n\n" . str_repeat('-', 25 ) . ' STARTING DEBUG [' . date('h:i:sa', current_time('timestamp') ) . '] ' . str_repeat('-', 25 ) . "\n\n" );
+	  error_log( "\n" . $counter . '. ' . basename( $caller['file'] ) . '::' . $caller['line'] . "\n" . $message . "\n---\n" );
+	  $counter++;
+	}
+}
+
 $mailtrap = dirname( __FILE__ ) . '/mailtrap.php';
 if( file_exists( $mailtrap ) )
     require( $mailtrap );
