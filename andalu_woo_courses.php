b74e366ed471fd6a2a8f11c8a2e3b78856a48f61
Michael Wender <michael@michaelwender.com>
2021-03-03 15:53:15 -0500

    Additional Spanish translations

--- andalu_woo_courses.php
+++ andalu_woo_courses.php
@@ -3,7 +3,7 @@
 Plugin Name: ANDA.lu Woo Courses
 Plugin URI: http://anda.lu/design
 Description: Adds a custom WC Product type called Courses
-Version: 2.6.6
+Version: 2.6.7
 Author: ANDA.lu
 Author URI: http://anda.lu/design
 */
