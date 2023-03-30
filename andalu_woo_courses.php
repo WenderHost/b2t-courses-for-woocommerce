203e448047a95dea38f7f3e34a5c39246f89cfc7
Michael Wender <michael@michaelwender.com>
2023-03-30 17:24:44 -0400

    Documenting 3.1.7

--- andalu_woo_courses.php
+++ andalu_woo_courses.php
@@ -3,7 +3,7 @@
 Plugin Name: ANDA.lu Woo Courses
 Plugin URI: https://github.com/WenderHost/andalu_woo_courses
 Description: Adds a custom WC Product type called Courses
-Version: 3.1.6
+Version: 3.1.7
 Author: ANDA.lu
 Author URI: http://anda.lu/design
 */
